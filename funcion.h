6254340b3762c7dc88feac6f0d3485f6f9580c27
mauriciogiraldou <mauricio.giraldou@udea.edu.co>
2024-03-27 17:35:58 -0500

    Agregadas funciones crearmatrizimpar, rotarmatriz, y liberarmatriz

--- funcion.h
+++ funcion.h
@@ -3,5 +3,7 @@
 #include <iostream>
 using namespace std;
 int queseaimpar(int n);
-
+int** crearMatrizImpar(int n);
+void rotateMatrix(int** mat, int n);
+void liberarMatriz(int** matriz, int n);
 #endif // FUNCION_H
--- funciones.cpp
+++ funciones.cpp
@@ -6,3 +6,46 @@ int queseaimpar(int Numeroi) {
     }
     return Numeroi;
 }
+void rotateMatrix(int** mat, int n) {
+    // Transpose the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+
+    // Reverse each row of the matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            swap(mat[i][j], mat[i][n - j - 1]);
+        }
+    }
+}
+int** crearMatrizImpar(int n) {
+    int** matriz = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        matriz[i] = new int[n];
+    }
+
+    int num = 1;
+    int centro = n / 2; // Índice del elemento central
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == centro && j == centro) {
+                // Elemento central vacío
+                matriz[i][j] = 0;
+            } else {
+                matriz[i][j] = num++;
+            }
+        }
+    }
+
+    return matriz;
+}
+void liberarMatriz(int** matriz, int n) {
+    for (int i = 0; i < n; ++i) {
+        delete[] matriz[i];
+    }
+    delete[] matriz;
+}
